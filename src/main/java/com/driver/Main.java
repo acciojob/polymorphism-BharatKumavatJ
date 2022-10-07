b5791d103c59a13105ee4b84645993a1462c28d0
bharat <bharatkumarchuahan179@gmail.com>
2022-10-07 20:19:38 +0530

    function overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,19 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public int product(int x, int y) {  return 0;}
+        public int product(int x, int y, int z) { return 1;}
+        public double product(double x, double y) {
+            return 0.0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(1, 2);
+        int ans2 = p.product(1, 2, 3);
+        double ans3 = p.product(1.2, 2.1);
+    }
 }
